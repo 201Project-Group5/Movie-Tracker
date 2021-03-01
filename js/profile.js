6f9f381f0b702058cc04d3c21ef17f9c9d3b8ec2
Reema Eilouti <reema.eilouti@gmail.com>
2021-03-01 16:23:22 +0200

    fixed concat favMovies

--- js/profile.js
+++ js/profile.js
@@ -1,34 +1,34 @@
 function populateTable() {
 
-  let tableEl = document.getElementById('favoriteList');
-  let tableBodyEl = document.getElementById('tbody1');
+    let tableEl = document.getElementById('favoriteList');
+    let tableBodyEl = document.getElementById('tbody1');
 
-  aflamy = JSON.parse(localStorage.getItem('favMovies'));
+    let aflamy = JSON.parse(localStorage.getItem('favMovies'));
 
-  for (let i in aflamy) {
-    const tableRowEl = document.createElement('tr');
-    tableBodyEl.appendChild(tableRowEl);
+    for (let i in aflamy) {
+        const tableRowEl = document.createElement('tr');
+        tableBodyEl.appendChild(tableRowEl);
 
-    let td1El = document.createElement('td');
-    let td2El = document.createElement('td');
-    let td3El = document.createElement('td');
-    let td4El = document.createElement('td');
-    let td5El = document.createElement('td');
-    let imgEl = document.createElement('img');
+        let td1El = document.createElement('td');
+        let td2El = document.createElement('td');
+        let td3El = document.createElement('td');
+        let td4El = document.createElement('td');
+        let td5El = document.createElement('td');
+        let imgEl = document.createElement('img');
 
-    td1El.textContent = 'X';
-    td2El.textContent = aflamy[i].title;
-    td3El.textContent = aflamy[i].year;
-    td4El.textContent = aflamy[i].category;
-    imgEl.src = aflamy[i].poster;
+        td1El.textContent = 'X';
+        td2El.textContent = aflamy[i].title;
+        td3El.textContent = aflamy[i].year;
+        td4El.textContent = aflamy[i].category;
+        imgEl.src = aflamy[i].poster;
 
-    tableRowEl.appendChild(td1El);
-    tableRowEl.appendChild(td2El);
-    tableRowEl.appendChild(td3El);
-    tableRowEl.appendChild(td4El);
-    td5El.appendChild(imgEl);
-    tableRowEl.appendChild(td5El);
-  }
+        tableRowEl.appendChild(td1El);
+        tableRowEl.appendChild(td2El);
+        tableRowEl.appendChild(td3El);
+        tableRowEl.appendChild(td4El);
+        td5El.appendChild(imgEl);
+        tableRowEl.appendChild(td5El);
+    }
 
 }
 populateTable();
